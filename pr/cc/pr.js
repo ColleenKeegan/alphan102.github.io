7488af16d27329c4bbeb35e840521fb55f001bb2
Alphan Chen <alchen@mozilla.com>
2017-03-17 11:44:21 +0800

    Test Constructor only
    Round 1: Without supportedMethods
    Round 2: detail.total.amount.value is negative number
    Round 3: detail.displayItems.amount.valie is invalid decimal monetary value
    Round 4: Pass all checks

--- pr/cc/pr.js
+++ pr/cc/pr.js
@@ -2,6 +2,54 @@
 /* global error:false */
 /* global PaymentRequest:false */
 
+var Round = 1;
+
+var supportedInstruments = [{}]; //[{supportedMethods:[]}];
+
+var details = {
+  total: {
+    label: 'Donation',
+    amount: {
+      currency: 'USD',
+      value: '-55.00'
+    }
+  },
+  displayItems: [{
+    label: 'Original donation amount',
+    amount: {
+      currency: 'USD',
+      value: '-65.00'
+    }
+  }, {
+    label: 'Friends and family discount',
+    amount: {
+      currency: 'USD',
+      value: 'aaa10.00'
+    }
+  }],
+  modifiers: [{
+    supportedMethods: ['visa'],
+    total: {
+      label: 'Discounted donation',
+      amount: {
+        currency: 'USD',
+        value: '45.00'
+      }
+    },
+    additionalDisplayItems: [{
+      label: 'VISA discount',
+      amount: {
+        currency: 'USD',
+        value: '-10.00'
+      }
+    }],
+    data: {
+      discountProgramParticipantId: '86328764873265'
+    }
+  }]
+};
+
+
 /**
  * Initializes the payment request object.
  */
@@ -10,85 +58,65 @@ function buildPaymentRequest() {
     return null;
   }
 
-  var supportedInstruments = [{
-    supportedMethods: [
-      'unionpay', 'visa', 'mastercard', 'amex', 'discover', 'diners', 'jcb'
-    ]
-  }];
-
-  var details = {
-    total: {
-      label: 'Donation',
-      amount: {
-        currency: 'USD',
-        value: '55.00'
-      }
-    },
-    displayItems: [{
-      label: 'Original donation amount',
-      amount: {
-        currency: 'USD',
-        value: '65.00'
-      }
-    }, {
-      label: 'Friends and family discount',
-      amount: {
-        currency: 'USD',
-        value: '-10.00'
-      }
-    }],
-    modifiers: [{
-      supportedMethods: ['visa'],
-      total: {
-        label: 'Discounted donation',
-        amount: {
-          currency: 'USD',
-          value: '45.00'
-        }
-      },
-      additionalDisplayItems: [{
-        label: 'VISA discount',
-        amount: {
-          currency: 'USD',
-          value: '-10.00'
-        }
-      }],
-      data: {
-        discountProgramParticipantId: '86328764873265'
-      }
-    }]
-  };
-
   var request = null;
 
   try {
     request = new PaymentRequest(supportedInstruments, details);
     if (request.canMakePayment) {
+      /*
       request.canMakePayment().then(function(result) {
         info(result ? "Can make payment" : "Cannot make payment");
       }).catch(function(err) {
         error(err);
-      });
+      });*/
     }
   } catch (e) {
     error('Developer mistake: \'' + e + '\'');
+    if (Round == 1) {
+      supportedInstruments = [{
+        supportedMethods: [
+          'unionpay', 'visa', 'mastercard', 'amex', 'discover', 'diners', 'jcb'
+        ]
+      }];
+    } else if (Round == 2) {
+      details.total.amount = {
+        currency: 'USD',
+        value: '55.00'
+      };
+    } else if(Round == 3) {
+      details.displayItems = [{
+        label: 'Original donation amount',
+        amount: {
+          currency: 'USD',
+          value: '-65.00'
+        }
+      }, {
+        label: 'Friends and family discount',
+        amount: {
+          currency: 'USD',
+          value: '10.00'
+        }
+      }];
+    }
   }
 
   return request;
 }
 
-var request = buildPaymentRequest();
-
 /**
  * Launches payment request for credit cards.
  */
 function onBuyClicked() { // eslint-disable-line no-unused-vars
+  var request = buildPaymentRequest();
   if (!window.PaymentRequest || !request) {
-    error('PaymentRequest API is not supported.');
+    error('Round:' + Round + '-PaymentRequest API is not supported.');
+    Round++;
     return;
   }
-
+  error('Round:' + Round + '-Got request object.');
+    /*
   try {
+
     request.show()
       .then(function(instrumentResponse) {
         window.setTimeout(function() {
@@ -109,5 +137,5 @@ function onBuyClicked() { // eslint-disable-line no-unused-vars
   } catch (e) {
     error('Developer mistake: \'' + e + '\'');
     request = buildPaymentRequest();
-  }
+  }*/
 }
